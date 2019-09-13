a6312ed7500b562f330b67774cdd1b5edb05ff58
Kirill Kamakin <k.kamakin@protonmail.ch>
2019-09-14 01:05:31 +0300

    Add scheme as parameter to procedure
    Signed-off-by: Kirill Kamakin <k.kamakin@protonmail.ch>

--- lab1/script.sql
+++ lab1/script.sql
@@ -1,33 +1,33 @@
-create or replace procedure variables
-is
+CREATE OR REPLACE Procedure variables(schemeName in varchar2)
+IS 
 obj_type varchar(100);
 
-
 BEGIN
-DBMS_OUTPUT.enable(800000);
-DBMS_OUTPUT . PUT_LINE ('Имя схемы : s174692 ');
+	DBMS_OUTPUT.enable(800000);
+	DBMS_OUTPUT.PUT_LINE ('Имя схемы: ' || schemeName);
 
-DBMS_OUTPUT . PUT_LINE('No . Имя Пакета Тип объекта Имя объекта ' );
-DBMS_OUTPUT . PUT_LINE('____ ____________ ____________ ___________');
+	DBMS_OUTPUT.PUT_LINE('No . Имя Пакета Тип объекта Имя объекта');
+	DBMS_OUTPUT.PUT_LINE('____ ____________ ____________ ___________');
 
-for row in ( SELECT rownum , object_name , type , name from user_ identifiers 
-            WHERE object_type in ( 'PACKAGE' , 'PACKAGE BODY' ) 
-            and usage='DECLARATION' and type in
-            ('VARIABLE' , 'CONSTANT' ) and usage_context_id in 
-            ( SELECT usage_id FROM
-            user_identifiers WHERE type ='PACKAGE' ))
- loop
- if row . type = 'VARIABLE' then
- obj_type := Переменная ' ';
-end if;
+	FOR row IN (SELECT rownum, object_name, type, name from all_identifiers 
+		WHERE owner = schemeName
+		AND object_type in ('PACKAGE', 'PACKAGE BODY') 
+		AND usage = 'DECLARATION' 
+		AND type IN ('VARIABLE', 'CONSTANT') 
+		AND usage_context_id IN 
+			(SELECT usage_id FROM all_identifiers 
+			WHERE type ='PACKAGE'))
+	LOOP
+		IF row.type = 'VARIABLE' THEN
+ 			obj_type := 'Переменная';
+		END IF;
 
- if row . type = 'CONSTANT' then
-obj_type := Константа ' ';
- end if ;
+		IF row.type = 'CONSTANT' THEN
+			obj_type := 'Константа';
+		END IF;
 
-DBMS_OUTPUT . PUT_LINE(RPAD( row . rownum , 4 ) | | ' ' | | RPAD( row . object_name , 12 ) | |
-' ' | | RPAD( obj_type , 12 ) | | ' ' | | RPAD( row . name , 30 ) ) ;
-end loop;
+		DBMS_OUTPUT.PUT_LINE(RPAD(row.rownum, 4) || ' ' || RPAD(row.object_name, 12) || ' ' || RPAD(obj_type, 12) || ' ' || RPAD(row.name, 30));
+	END LOOP;
 
- END
+END;
 /
