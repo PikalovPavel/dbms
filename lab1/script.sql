5e607607fb86681be4e41576efb523f34b3e3c60
Pavel <pp@pqlab.ru>
2019-09-18 16:14:13 +0300

    minor improvents

--- lab1/script.sql
+++ lab1/script.sql
@@ -1,33 +1,28 @@
-CREATE OR REPLACE Procedure variables(schemeName in varchar2)
-IS 
-obj_type varchar(100);
-
+SET SERVEROUTPUT ON;
+CREATE OR REPLACE PROCEDURE VARIABLES(schemeName IN varchar2)
+IS
+obj_type VARCHAR(100);
 BEGIN
-	DBMS_OUTPUT.enable(800000);
-	DBMS_OUTPUT.PUT_LINE ('Имя схемы: ' || schemeName);
-
-	DBMS_OUTPUT.PUT_LINE('No . Имя Пакета Тип объекта Имя объекта');
-	DBMS_OUTPUT.PUT_LINE('____ ____________ ____________ ___________');
-
-	FOR row IN (SELECT rownum, object_name, type, name from all_identifiers 
-		WHERE owner = schemeName
-		AND object_type in ('PACKAGE', 'PACKAGE BODY') 
-		AND usage = 'DECLARATION' 
-		AND type IN ('VARIABLE', 'CONSTANT') 
-		AND usage_context_id IN 
-			(SELECT usage_id FROM all_identifiers 
-			WHERE type ='PACKAGE'))
-	LOOP
-		IF row.type = 'VARIABLE' THEN
- 			obj_type := 'Переменная';
-		END IF;
-
-		IF row.type = 'CONSTANT' THEN
-			obj_type := 'Константа';
-		END IF;
-
-		DBMS_OUTPUT.PUT_LINE(RPAD(row.rownum, 4) || ' ' || RPAD(row.object_name, 12) || ' ' || RPAD(obj_type, 12) || ' ' || RPAD(row.name, 30));
-	END LOOP;
-
+        DBMS_OUTPUT.enable(800000);
+        DBMS_OUTPUT.PUT_LINE('Имя схемы: ' || schemeName);
+        DBMS_OUTPUT.PUT_LINE('No . Имя Пакета Тип объекта Имя объекта');
+        DBMS_OUTPUT.PUT_LINE('____ __________ ___________ ___________');
+        FOR ROW IN (SELECT rownum, object_name, type, name FROM all_identifiers
+                WHERE owner = schemeName
+                AND object_type IN ('PACKAGE', 'PACKAGE BODY')
+                AND usage = 'DECLARATION'
+                AND TYPE IN ('VARIABLE', 'CONSTANT')
+                AND usage_context_id IN
+                        (SELECT usage_id FROM all_identifiers
+                        WHERE TYPE ='PACKAGE'))
+        LOOP
+                IF ROW.type = 'VARIABLE' THEN
+                        obj_type := 'Переменная';
+                END IF;
+                IF ROW.type = 'CONSTANT' THEN
+                        obj_type := 'Константа';
+                END IF;
+        DBMS_OUTPUT.PUT_LINE(RPAD(ROW.rownum, 4) || ' ' || RPAD(ROW.object_name, 12) || ' ' || RPAD(obj_type, 12) || ' ' || RPAD(ROW.name,30));
+END LOOP;
 END;
 /
